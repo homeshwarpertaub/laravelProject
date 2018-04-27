c175723de8e4ec4c8eaa4c65a434bcf88ee28318
homeshwarpertaub <onefortheroad02@gmail.com>
2018-04-26 18:57:07 -0700

    upload file dont know what change i made

--- resources/views/admin/applicants.blade.php
+++ resources/views/admin/applicants.blade.php
@@ -1,33 +1,91 @@
 @extends('admin.layouts.app')
+@section('headSection')
+
+
+@endsection
+
 
 @section('main-content')
     <div class="container">
-        <div class="row">
-            <div class="col-md-12">
+        <div class="row m-t-10">
+
+            <div class="col-md-8">
                 @foreach($applicants as $applicant)
-                    <h4>Applicant: </h4>
-                    {{ $applicant -> applicant_surname }}
-                    {{ $applicant -> applicant_dob }}
-                    <h4>Course Choice</h4>
-                    @foreach($applicant->courseChoice  as $courseChoice)
 
-                        {{ $courseChoice -> title1 }}
-                        @foreach($applicant->institution as $institution)
-                            {{ $institution-> inst_name1 }}
+                <div class="panel panel-inverse">
+                    <div class="panel-heading">{{ $applicant -> id }}
+                        <div class="panel-action">
+                            <div class="dropdown">
+                                <a href="#" aria-expanded="false" data-toggle="dropdown" role="button"
+                                   class="dropdown-toggle" id="panelDropdown">Options <span class="caret"></span></a>
+                                <ul class="dropdown-menu bullet dropdown-menu-right" aria-labelledby="panelDropdown"
+                                    role="menu">
+                                    <li role="presentation"><a href="#" role="menuitem">View PDF</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="panel-wrapper collapse in" aria-expanded="true">
+                        <div class="panel-body">
+                                <div class="white-box">
+                                    <div class="table-responsive">
+                                        <table class="table">
+                                            <thead>
+                                            <tr>
+                                                <th>Surname</th>
+                                                <th>Other Name</th>
+                                                <th>Sex</th>
+                                                <th>Nationality</th>
+                                                <th>Email</th>
+                                                <th>Photo</th>
+                                            </tr>
+                                            </thead>
+                                            <tbody>
+                                            <tr>
+                                                <td>{{ $applicant -> applicant_surname }}
+                                                </td>
+                                                <td>{{ $applicant -> applicant_otherName }}</td>
+                                                <td>{{ $applicant -> applicant_sex }}</td>
+                                                <td>{{ $applicant -> applicant_nationality }}</td>
+                                                <td>{{ $applicant -> applicant_email }}</td>
+                                                <td></td>
+                                            </tr>
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                    @foreach($applicant->courseChoice  as $courseChoice)
+                                        <h3 class="box-title">
+                                            <ol>
+                                                <li>{{ $courseChoice -> title1 }}
+                                                </li>
+                                                <li>{{ $courseChoice -> title2 }}</li>
+                                                <li>{{ $courseChoice -> title3 }}</li>
+                                            </ol>
+                                        </h3>
+                                </div>
 
-                            <p>
-                                {{--{{dump($applicantt)}}--}}
-                            </p>
-                            <br>
+                                {{--@foreach($applicant->institution as $institution)--}}
+                                    {{--{{ $institution-> inst_name1 }}--}}
 
-                        @endforeach
-                    @endforeach
+                        </div>
+                    </div>
+                </div>
+
+                <p>
+                    {{--{{dump($applicantt)}}--}}
+                </p>
+                <br>
+
                 @endforeach
+                @endforeach
+                {{--@endforeach--}}
 
             </div>
         </div>
     </div>
+@endsection
 
+@section('footerSection')
 
 
 @endsection
